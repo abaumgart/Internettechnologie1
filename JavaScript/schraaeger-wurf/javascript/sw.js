0e55f16f1547ea3c56715d8d24d957a024fbec67
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2025-05-27 09:41:00 +0200

    update

--- JavaScript/schraaeger-wurf/javascript/sw.js
+++ JavaScript/schraaeger-wurf/javascript/sw.js
@@ -3,7 +3,31 @@
 function berechnen()
 {
 	console.log("Button wurde gedrückt!");
- 	//seiteA = document.getElementById('seiteA').value;
+ 	winkel = document.getElementById('winkel').value;
+	console.log(winkel);
+	v0 = document.getElementById('v0').value;
+	console.log(v0);
+	
+	// Eingaben in Input-Feldern sind Zeichenketten.
+	// Deshalb ist eine Umwandlung in numerische werte notwendig
+	
+	winkel = parseInt(winkel);
+	v0= parseInt(v0);
+	console.log(winkel);
+	console.log(v0);
+	
+	// Winkel umrechnen in radiant (mit Math.PI)
+	// winkel * (pi/180)
+	
+	winkel = winkel*(Math.PI/180);
+	console.log(winkel);
+	// Wurfweite berechnen Math.sin()
+	
+	wurfweite= (v0*v0 * Math.sin(2*winkel))/9.81;
+	console.log (wurfweite);
+	
+	
+	
 	//seiteB = document.getElementById('seiteB').value;
 	//flaeche = seiteA*seiteB;
 	// alert(flaeche);
