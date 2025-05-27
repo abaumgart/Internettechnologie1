d8e03bb3508885cd5c5749138514e0a206197c44
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2025-05-27 08:47:15 +0200

    update

---
+++ JavaScript/schraaeger-wurf/javascript/sw.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function berechnen()
+{
+	console.log("Button wurde gedrückt!");
+ 	//seiteA = document.getElementById('seiteA').value;
+	//seiteB = document.getElementById('seiteB').value;
+	//flaeche = seiteA*seiteB;
+	// alert(flaeche);
+	//document.getElementById('ergebnis').value=flaeche;
+}
